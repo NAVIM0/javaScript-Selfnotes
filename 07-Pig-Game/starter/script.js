8d95d1117582f26e59651f27f7de671fcfe15cff
Navid Mahjoub <88707804+NAVIM0@users.noreply.github.com>
2024-07-22 04:28:55 +0330

    Finished Section 08

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,20 +1,52 @@
 // @flow
 'use strict';
 
+//some variables for the ease of access
+const player0Element: ?HTMLElement = document.querySelector('.player--0');
+const player1Element: ?HTMLElement = document.querySelector('.player--1');
+const btnCloseModalElement: ?HTMLElement = document.querySelector('.close-modal');
+const modalElement: ?HTMLElement = document.querySelector('.modal');
+const overlayElement: ?HTMLElement = document.querySelector('.overlay');
+const diceElement: ?HTMLImageElement = (document.querySelector('.dice'): any);
+const btnRollDice: ?HTMLElement = document.querySelector('.btn--roll');
+const current0scoreElement: ?HTMLElement = document.getElementById('score--0');
+const current1scoreElement: ?HTMLElement = document.getElementById('score--1');
+const temp0score: ?HTMLElement = document.getElementById('current--0');
+const temp1score: ?HTMLElement = document.getElementById('current--1');
+const newBtnElement: ?HTMLElement = document.querySelector('.btn--new');
+const btnHoldElement: ?HTMLElement = document.querySelector('.btn--hold');
+const confirmBtnElement: ?HTMLElement = document.querySelector('.confirm');
+const cancelBtnElement: ?HTMLElement = document.querySelector('.cancel');
+let currentScore = 0;
+let currentPlayer = 0;
+let isPlaying = true;
+
+
+function endGame() {
+  if (btnRollDice) btnRollDice.style.display = 'none'; else console.error("element Not Found");
+  if (btnHoldElement) btnHoldElement.style.display = 'none'; else console.error("element Not Found");
+  if (diceElement) diceElement.style.display = 'none'; else console.error("element Not Found");
+  isPlaying = false;
+}
+
+//util functions
 function resetGame() {
 
-  if (modalElement) modalElement.classList.remove('hidden');
-  if (overlayElement) overlayElement.classList.remove('hidden');
-  currentPlayer = 0;
   currentScore = 0;
   if (diceElement) diceElement.classList.add('hidden');
   if (temp1score) temp1score.textContent = '0';
   if (temp0score) temp0score.textContent = '0';
   if (current0scoreElement) current0scoreElement.textContent = '0';
   if (current1scoreElement) current1scoreElement.textContent = '0';
+  if (btnRollDice) btnRollDice.style.display = 'initial'; else console.error("element Not Found");
+  if (btnHoldElement) btnHoldElement.style.display = 'initial'; else console.error("element Not Found");
+  if (player1Element) player1Element.classList.remove("player--loser");
+  if (player0Element) player0Element.classList.remove("player--loser");
+  isPlaying = true;
 }
 
 function closeModal() {
+
   if (modalElement) modalElement.classList.add('hidden'); else console.error('modal Not Found');
   if (overlayElement) overlayElement.classList.add('hidden'); else console.error('overlay Not Found');
 }
@@ -24,39 +56,22 @@ function switchPlayer() {
   currentScore = 0;
 
   if (currentPlayer) {
-
     if (temp1score) temp1score.textContent = '0';
-    currentPlayer--;
-    if(player0Element) player0Element.classList.add('player--active'); else console.error('player0Element Not Found');
-    if(player1Element) player1Element.classList.remove('player--active'); else console.error('player1Element Not Found');
+    currentPlayer = 0;
+    if (player0Element) player0Element.classList.toggle('player--active'); else console.error('player0Element Not Found');
+    if (player1Element) player1Element.classList.toggle('player--active'); else console.error('player1Element Not Found');
 
   } else {
-
     if (temp0score) temp0score.textContent = '0';
-    currentPlayer++;
-    if(player0Element) player0Element.classList.remove('player--active'); else console.error('player0Element Not Found');
-    if(player1Element) player1Element.classList.add('player--active'); else console.error('player1Element Not Found');
+    currentPlayer = 1;
+    if (player0Element) player0Element.classList.toggle('player--active'); else console.error('player0Element Not Found');
+    if (player1Element) player1Element.classList.toggle('player--active'); else console.error('player1Element Not Found');
   }
 }
 
-const player0Element :?HTMLElement = document.querySelector('.player--0');
-const player1Element :?HTMLElement = document.querySelector('.player--1');
-const btnCloseModalElement: ?HTMLElement = document.querySelector('.close-modal');
-const modalElement: ?HTMLElement = document.querySelector('.modal');
-const overlayElement: ?HTMLElement = document.querySelector('.overlay');
-const diceElement: ?HTMLImageElement = (document.querySelector('.dice'): any);
-const btnRollDice: ?HTMLElement = document.querySelector('.btn--roll');
-const current0scoreElement: ?HTMLElement = document.getElementById('score--0');
-const current1scoreElement: ?HTMLElement = document.getElementById('score--1');
-const newBtnElement: ?HTMLElement = document.querySelector('.btn--new');
-const btnHoldElement: ?HTMLElement = document.querySelector('.btn--hold');
-const temp0score: ?HTMLElement = document.getElementById('current--0');
-const temp1score: ?HTMLElement = document.getElementById('current--1');
-const confirmBtnElement: ?HTMLElement = document.querySelector('.confirm');
-const cancelBtnElement: ?HTMLElement = document.querySelector('.cancel');
-let currentScore = 0;
-let currentPlayer = 0;
 
+
+//JavaScript Event Listeners
 if (btnRollDice) btnRollDice.addEventListener('click', () => {
 
   let dice = Math.trunc(Math.random() * 6 + 1);
@@ -87,61 +102,57 @@ if (btnRollDice) btnRollDice.addEventListener('click', () => {
 
 if (btnHoldElement) btnHoldElement.addEventListener('click', () => {
 
-  if (currentPlayer) {
+  if (player1Element && player0Element && current1scoreElement && current0scoreElement) {
 
-    if (current1scoreElement) {
+    if (currentPlayer) {
 
       let tempScore = Number(current1scoreElement.textContent) + currentScore;
-      current1scoreElement.textContent = tempScore >= 100 ? "Winner!" : String(tempScore);
+      current1scoreElement.textContent = (tempScore >= 100 || current1scoreElement.textContent === 'Winner!') ? 'Winner!' : String(tempScore);
 
-    } else console.error('element Not Found');
+      if (tempScore >= 100) {
 
-  } else {
-
-    if (current0scoreElement) {
+        player0Element.classList.add('player--loser');
+        endGame();
+      }
+    } else {
 
       let tempScore = Number(current0scoreElement.textContent) + currentScore;
-      current0scoreElement.textContent = tempScore >= 100 ? "Winner!" : String(tempScore);
+      current0scoreElement.textContent = (tempScore >= 100 || current0scoreElement.textContent === 'Winner!') ? 'Winner!' : String(tempScore);
 
-    } else console.error('element Not Found');
-  }
-  switchPlayer();
+      if (tempScore >= 100) {
+
+        player1Element.classList.add('player--loser');
+        endGame();
+      }
+    }
+  } else console.error('element(s) not found');
+
+  if(isPlaying) switchPlayer();
 
 }); else console.error('element Not Found');
 
 
 if (newBtnElement) newBtnElement.addEventListener('click', () => {
 
-  resetGame();
+  if (modalElement) modalElement.classList.remove('hidden');
+  if (overlayElement) overlayElement.classList.remove('hidden');
 
 }); else console.error('element Not Found');
 
-
-if (btnCloseModalElement) btnCloseModalElement.addEventListener('click', () => {
-  closeModal();
-
-}); else console.error('overlay Not Found');
-
+if (btnCloseModalElement) btnCloseModalElement.addEventListener('click', closeModal); else console.error('overlay Not Found');
 
 document.addEventListener('keydown', (e: KeyboardEvent) => {
 
   if (modalElement && e.code === 'Escape' && !modalElement.classList.contains('hidden')) {
     closeModal();
   }
-
 });
 
-if (overlayElement) overlayElement.addEventListener('click', () => {
-  closeModal();
-});
+if (overlayElement) overlayElement.addEventListener('click', closeModal);
 
 if (confirmBtnElement) confirmBtnElement.addEventListener('click', () => {
   resetGame();
   closeModal();
 });
 
-if (cancelBtnElement) cancelBtnElement.addEventListener('click', () => {
-  closeModal();
-});
-
-
+if (cancelBtnElement) cancelBtnElement.addEventListener('click', closeModal);
--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,278 @@
+// @flow
 'use strict';
+
+//by testing,
+// you can clearly see that JavaScript is interpreted
+// and JIT-compiled only in some modern browsers like chrome using the V8 engine!
+//for optimization purposes!
+
+//like this code:
+/*
+  function sayHello(name) {
+
+  name.charAt(0);
+  }
+
+  console.log("js is");
+  console.log("definitely");
+  console.log("interpreted");
+
+  name = [32,45,5,6645];
+
+  sayHello(name);
+ */
+
+// also that dom manipulation and some other super Important stuff are not part of js
+// and are api are provided by browsers!
+
+function calcAge(birthYear : number) {
+
+  //function Scope
+  let currentDate = new Date();
+  const age = currentDate.getFullYear() - birthYear;
+
+
+  function printAge () {
+    const output = `${firstName} is ${age} and is Born in ${birthYear}`;
+    console.log(output);
+
+    
+    if (birthYear >= 1981 && birthYear <= 1996) {
+
+
+      //hoisting of variable is mostly the same in java too except the "undefined" of var variables!
+
+      //won't work and is *uninitialized* since lastName is defined later,
+      // and let and const variables aren't hoisted and so it's in TDZ (Temporal Dead Zone)
+      // if (lastName) {
+      //
+      // }
+
+      // const lastName = "mahjoub"
+
+      const firstName = "scott";
+
+
+      //var variables aren't hoisted and just become *undefined* if accessed before their declaration!
+      //var is function scoped!
+      // ->
+      // not block scoped
+      var millennial = true;
+
+
+      //since a firstName is defined in this scope already there will be no variable look-up
+      //and it wil just use the one in this scope!!!
+      const message = `inside the block scope the firstName is: ${firstName}`;
+      console.log(message);
+
+
+      function add(a:number, b: number) {
+
+        return a + b;
+      }
+
+      //add function can only be used in this block in strict mode!
+      console.log(add(2,3));
+    }
+
+    //since the variable firstName isn't defined in this function scope, there will be a variable lookup
+    //that will result in using the global scope variable first name being used and hence the first name
+    //will be "John"!
+    console.log(`outside the block scope the firstName is: ${firstName}`);
+
+
+    /*
+    VERY IMPORTANT INFORMATION:
+
+      Code like the above example (Two FirstName Variables in one scope) won't be excepted in
+      other languages such as java...since two variables with the same name Can't exist in a single scope!
+
+     */
+
+    
+    //add function will not be recognized since functions are block scoped in strict mode!
+    //add(3,4);
+    console.log(millennial);
+  }
+
+  printAge();
+  return age;
+}
+
+//global scope
+let firstName = 'John';
+calcAge(1986);
+
+
+
+
+
+
+//variable hoisting
+
+
+console.log(me);
+//console.log(job);
+//console.log(year);
+
+
+
+var me = "jonas";
+let job = "teacher";
+const year = 1991;
+
+
+
+
+//function hoisting
+
+console.log(addDecl(2,3));
+// console.log(addExpr(2,3));
+// console.log(addArrow(2,3));
+
+
+//hoisting only works for function declaration!
+function addDecl(a : number, b: number) {
+  return a + b;
+}
+
+
+
+
+//arguments keyword!
+const addExpr = function(a: number, b: number) {
+
+  console.log(arguments);
+
+  return a + b;
+}
+
+addExpr(2,5);
+
+const addArrow = (a: number,b: number) => a + b;
+
+
+
+//*** THIS KEYWORD ****//
+
+
+//also around the subject of the "this" keyword...it simply references the object calling it!
+//in JavaScript, you can copy an objects function by doing => "let f = jonas.clacAge;"
+//and the value of the function by doing => "let f = jonas.calcAge();"
+
+//also notice that if the copied method is the borrowed by another object, the "this" keyword used inside
+//the borrowed function will belong to the object calling the function like this:
+
+//navid.calcAge = f
+//navid.calcAge(); ==> the this keyword will refer to the navid object
+
+//also arrow function's this keyword belongs to the parent scope unlike a regular function
+//if no object is calling a method, then the "this" keyword used inside that method will be undefined
+//even if it's called inside an object!
+
+//except if the function call calls an arrow function, in which case the arrow function just uses its parent scope's this
+
+
+type module = {
+  birthYear: number,
+  calcAge?: () => number
+}
+
+type alternativeType = {
+  [string]:mixed
+
+  //could add + or - before [String] to specify Read-Only and Write-Only
+}
+
+let navid = {
+  birthYear: 2003,
+
+  calcAge: function(this: {birthYear : number,...}) {
+
+    //only works if it's an arrow function!
+    let isYoung = () => {
+      console.log(this);
+      if (this.birthYear < 30) {
+
+        console.log("He is young!");
+      }
+    };
+    isYoung();
+    return 2024 - this.birthYear;
+  },
+};
+
+let omid : module = {
+  birthYear: 1998,
+}
+
+//don't need the || because exact_by_default is on in .flowConfig
+let omid2 : {|birthYear: number|} = {
+  birthYear: 1998
+}
+
+console.log(navid.calcAge());
+
+omid.calcAge = navid.calcAge;
+
+//this will refer to Omid
+console.log(omid.calcAge());
+
+
+
+
+
+
+//javaScript & Java are pass-by-value since the value of the passed
+// argument to the function(which is the address of "object" in heap)
+// wasn't changed for the passed variable to the changeValue Function
+//and navid's actual value which is the address of the object literal that it contains the address of
+// stays the same after the function is called
+// as seen by the result of console.log
+
+//note that the same logic applies for variables that contain primitive types instead of object addresses!
+function changeValue(object:module) {
+
+  return object = omid;
+}
+
+
+console.log(changeValue(navid));
+console.log(navid);
+
+
+//languages like c++ can pass the actual reference to the function which will result in
+//potential change to the initial value of the variable passed unlike what we see here!
+
+
+//C is kinda pass by reference by some trickery with address and dereferencing of variables!
+
+
+
+
+//JavaScript being Pass-By-Value Example!
+function swap(a: number, b: number) {
+  let temp = a;
+  a = b;
+  b = temp;
+
+  //the variables created temporarily for this execution context are changed but not the OG Variables!
+  console.log(a,b);
+}
+
+let a = 3;
+let b = 5;
+
+swap(a,b);
+console.log(a,b);
+
+
+//COPY AN OBJECT -> won't work on a nested object...it's a shallow copy, not a deep clone!
+//{} was marked as a dictionary of string: number which is what our object literals are basically
+const navidCopy = Object.assign(({}: {[string]:mixed}),navid);
+
+navidCopy.birthYear = 2005;
+
+console.log(navid.birthYear);
+console.log(navidCopy.birthYear);
+
